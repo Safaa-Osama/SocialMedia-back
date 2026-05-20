576bb59f8adb2ac9d0501f59af21aad25f198633
Safaa-Osama <safaaosama386@gmail.com>
2026-05-20 19:43:33 +0300

    update post status

--- src/modules/posts/post.services.ts
+++ src/modules/posts/post.services.ts
@@ -88,7 +88,7 @@ class PostService {
 
         if (search) {
             searchQuery = {
-                $or: [{   content: { $regex: search, $options: "i" }  } ]
+                $or: [{ content: { $regex: search, $options: "i" } }]
             }
         }
 
@@ -103,118 +103,124 @@ class PostService {
         });
 
         successResponse({ res, data: posts });
-};
+    };
 
 
-likeDislikePosts = async (req: Request, res: Response, next: NextFunction) => {
-    const { postId } = req.params
-    const { flag } = req.query
+    likeDislikePosts = async (req: Request, res: Response, next: NextFunction) => {
+        const { postId } = req.params
+        const { flag } = req.query
 
-    let updateQuery: any = {
-        $addToSet: { likes: req.user?._id }
-    }
-
-    if (flag === "disLike") {
-        updateQuery = {
-            $pull: { likes: req.user?._id }
+        let updateQuery: any = {
+            $addToSet: { likes: req.user?._id }
         }
-    }
 
-    const post = await this._postRepo.findOneAndUpdate({
-        filter: {
-            _id: postId,
-            ...postAvailability(req)
-        },
-        update: updateQuery
-    })
-
-    if (!post) {
-        throw new AppError("Post not found or not Authorized")
-    }
-
-    successResponse({ res, data: post })
-}
-
-updatePost = async (req: Request, res: Response, next: NextFunction) => {
-    const { content, attachments, availability, removeFiles, removeTags, tags, allowComment }: UpdatePostDto = req.params
-    const { postId } = req.params
-
-    const post = await this._postRepo.findOne({
-        filter: {
-            id: postId,
-            createdBy: req.user._id
-        }
-    })
-
-    if (!post) {
-        throw new AppError("Post not found or not authorized")
-    }
-
-    if (removeFiles?.length) {
-        const inValidFiles = removeFiles.filter((file: string) => {
-            return !post.attachments?.includes(file)
-        })
-        if (inValidFiles.length) {
-            throw new AppError("some of files not exist")
-        }
-        this._s3service.deleteManyFiles(removeFiles)
-
-        post.attachments = post.attachments?.filter((file: string) => {
-            return !removeFiles.includes(file)
-        }) as string[]
-    }
-
-    const updatedTags = new Set(post.tags?.map(id => id.toString()))
-    removeTags?.forEach((tag: string) => {
-        return updatedTags.delete(tag)
-    })
-
-    let fcmTokens: string[] = []
-    if (tags?.length) {
-        const menrionsTags = await this._userRepo.find({
-            filter: { _id: { $in: tags } }
-        })
-        if (tags.length != menrionsTags.length) {
-            throw new AppError("Some person you mention is not found")
-        }
-        for (const tag of menrionsTags) {
-            if (tag._id.toString() == req.user._id.toString()) {
-                throw new AppError("You can't mention your self")
+        if (flag === "disLike") {
+            updateQuery = {
+                $pull: { likes: req.user?._id }
             }
-            updatedTags.add(tag._id.toString());
-            (await this._redisService.getFCMs(tag._id)).map((token) => {
-                fcmTokens.push(token)
+        }
+
+        const post = await this._postRepo.findOneAndUpdate({
+            filter: {
+                _id: postId,
+                ...postAvailability(req)
+            },
+            update: updateQuery
+        })
+
+        if (!post) {
+            throw new AppError("Post not found or not Authorized")
+        }
+
+        successResponse({ res, data: post })
+    }
+
+    updatePost = async (req: Request, res: Response, next: NextFunction) => {
+        const { content, attachments, availability, removeFiles, removeTags, tags, allowComment }: UpdatePostDto = req.params
+        const { postId } = req.params
+
+        const post = await this._postRepo.findOne({
+            filter: {
+                id: postId,
+                createdBy: req.user._id
+            }
+        })
+
+        if (!post) {
+            throw new AppError("Post not found or not authorized")
+        }
+
+
+        if (removeFiles?.length) {
+            const inValidFiles = removeFiles.filter((file: string) => {
+                return !post.attachments?.includes(file)
+            })
+            if (inValidFiles.length) {
+                throw new AppError("some of files not exist")
+            }
+            this._s3service.deleteManyFiles(removeFiles)
+
+            post.attachments = post.attachments?.filter((file: string) => {
+                return !removeFiles.includes(file)
+            }) as string[]
+        }
+
+        const updatedTags = new Set(post.tags?.map(id => id.toString()))
+        removeTags?.forEach((tag: string) => {
+            return updatedTags.delete(tag)
+        })
+
+        let fcmTokens: string[] = []
+        if (tags?.length) {
+            const menrionsTags = await this._userRepo.find({
+                filter: { _id: { $in: tags } }
+            })
+            if (tags.length != menrionsTags.length) {
+                throw new AppError("Some person you mention is not found")
+            }
+            for (const tag of menrionsTags) {
+                if (tag._id.toString() == req.user._id.toString()) {
+                    throw new AppError("You can't mention your self")
+                }
+                updatedTags.add(tag._id.toString());
+                (await this._redisService.getFCMs(tag._id)).map((token) => {
+                    fcmTokens.push(token)
+                })
+            }
+            post.tags = [...updatedTags].map(id => new Types.ObjectId(id))
+        }
+
+        if (req.files?.length) {
+            if (!post.folderId) {
+                throw new AppError("Folder id not found")
+            }
+            let urls = await this._s3service.uploadFiles({
+                files: req.files as Express.Multer.File[],
+                path: `users/${req?.user?._id}/posts/${post.folderId!}`,
+                store_type: StoreEnum.memory
+            })
+
+            post.attachments?.push(...urls)
+        }
+
+        if (fcmTokens.length) {
+            await this._notificationService.sendNotifications({
+                tokens: fcmTokens,
+                data: {
+                    title: "new notification",
+                    body: "you have new mention"
+                }
             })
         }
-        post.tags = [...updatedTags].map(id => new Types.ObjectId(id))
-    }
-
-    if (req.files?.length) {
-        if (!post.folderId) {
-            throw new AppError("Folder id not found")
+        if (!content && !post.content &&
+            !attachments && !post.attachments?.length &&
+            req.files?.length == removeFiles?.length
+        ) {
+            throw new AppError("post will be empty, can't excute your update")
         }
-        let urls = await this._s3service.uploadFiles({
-            files: req.files as Express.Multer.File[],
-            path: `users/${req?.user?._id}/posts/${post.folderId!}`,
-            store_type: StoreEnum.memory
-        })
-
-        post.attachments?.push(...urls)
+        await post.save()
+        successResponse({ res, data: post })
     }
-
-    if (fcmTokens.length) {
-        await this._notificationService.sendNotifications({
-            tokens: fcmTokens,
-            data: {
-                title: "new notification",
-                body: "you have new mention"
-            }
-        })
-    }
-
-    await post.save()
-    successResponse({ res, data: post })
-}
 
 }
 
